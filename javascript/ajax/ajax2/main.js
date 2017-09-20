3009831484559dcc1c5a0af1c89024616e94b8d7
Michael Uman <Michael_Uman@sigmadesigns.com>
2017-09-20 14:26:15 -0700

    Fixes some AJAX code and adds refresh button
    * Moved code into window.onload event handler.
    * Load the bundle.js in the html head.
    * Fancy new Refresh button using styled CSS.

--- javascript/ajax/ajax2/main.js
+++ javascript/ajax/ajax2/main.js
@@ -4,35 +4,59 @@
 
 const tableify = require('tableify');
 
-let divElement = document.getElementById('output');
+window.onload = function (e) {
+    console.log("window.onload()");
 
-console.log("OK");
-divElement.innerHTML = "This is a test";
+    let divElement = document.getElementById('output');
 
-function getJSONData(onJsonLoad) {
-    let req = new XMLHttpRequest();
+    console.log("OK");
+    divElement.innerHTML = "This is a test";
 
+    document.getElementById('refreshButton').onclick = function (e) {
+        console.log("Refresh Button clicked!");
+        refreshList();
+        return false;
+    };
 
-    req.onreadystatechange = function() {
-        if ((req.readyState == 4) && (req.status == 200)) {
-            console.log("received AJAX data!");
-            let jsonData = JSON.parse(req.responseText);
-            onJsonLoad(jsonData);
-        }
-        //console.log("state change");
+    function getJSONData() {
+        return new Promise((res, rej) => {
+            let req = new XMLHttpRequest();
+
+            req.onerror = function(e) {
+                console.log("request error!");
+                rej(e);
+            }
+
+            req.onreadystatechange = function() {
+                if ((req.readyState == 4) && (req.status == 200)) {
+                    console.log("received AJAX data!");
+                    let jsonData = JSON.parse(req.responseText);
+    //                onJsonLoad(jsonData);
+                    res(jsonData);
+                }
+                //console.log("state change");
+            }
+
+            req.open("GET", "http://localhost:3000/config/files", true);
+            req.send();
+        });
     }
 
-    req.open("GET", "http://localhost:3000/config/files", true);
-    req.send();
+    function refreshList() {
+        /** Request JSON data from the config-server */
+        getJSONData().then((configJson) => {
+            let htmlTable = tableify(configJson, [ 'fileName', "description" ]);
+            console.log("loaded");
+            //console.log(configJson);
+
+            divElement.innerHTML = htmlTable;
+
+            //console.log(tableify(configJson));
+        }).catch((e) => {
+            console.log("Caught promise rejection!");
+            divElement.innerHTML = "<center><h3>Unable to connect to config server</h3></center>";
+        });
+    }
+
+    refreshList();
 }
-
-/** Request JSON data from the config-server */
-getJSONData((configJson) => {
-    let htmlTable = tableify(configJson, [ 'fileName', "description" ]);
-    console.log("loaded");
-    //console.log(configJson);
-
-    divElement.innerHTML = htmlTable;
-
-    //console.log(tableify(configJson));
-});
