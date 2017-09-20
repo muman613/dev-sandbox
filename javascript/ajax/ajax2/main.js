14a309e768d262429dedec6c001215db478dc65d
Michael Uman <Michael_Uman@sigmadesigns.com>
2017-09-19 18:07:13 -0700

    Check in fixes to AJAX code to run in browser.
    * Added browserify and tableify to the project.
    * Added new CSS stylesheet.

--- javascript/ajax/ajax2/main.js
+++ javascript/ajax/ajax2/main.js
@@ -1,3 +1,9 @@
+/**
+ * Example AJAX load of JSON data from config-server
+ */
+
+const tableify = require('tableify');
+
 let divElement = document.getElementById('output');
 
 console.log("OK");
@@ -7,19 +13,26 @@ function getJSONData(onJsonLoad) {
     let req = new XMLHttpRequest();
 
 
-    req.addEventListener('load', onJsonLoad);
-    // req.addEventListener('load', () => {
-    //     console.log("on load!");
-    //     onJsonLoad(JSON.parse(this.responseText));
-    // });
-    // req.onreadystatechange = function() {
-    //     console.log("state change");
-    // }
+    req.onreadystatechange = function() {
+        if ((req.readyState == 4) && (req.status == 200)) {
+            console.log("received AJAX data!");
+            let jsonData = JSON.parse(req.responseText);
+            onJsonLoad(jsonData);
+        }
+        //console.log("state change");
+    }
 
-    req.open("GET", "http://localhost:3000/config/files");
+    req.open("GET", "http://localhost:3000/config/files", true);
     req.send();
 }
 
-getJSONData((res) => {
+/** Request JSON data from the config-server */
+getJSONData((configJson) => {
+    let htmlTable = tableify(configJson, [ 'fileName', "description" ]);
     console.log("loaded");
+    //console.log(configJson);
+
+    divElement.innerHTML = htmlTable;
+
+    //console.log(tableify(configJson));
 });
