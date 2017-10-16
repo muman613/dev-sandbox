a77b1bae5a68f3fd9496c4e956d34d6689176bdb
Michael Uman <Michael_Uman@sigmadesigns.com>
2017-10-15 21:53:43 -0700

    Use recursive-readdir to get list of files.

--- javascript/express/video-stream/getpathhash.js
+++ javascript/express/video-stream/getpathhash.js
@@ -4,8 +4,30 @@ const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 const md5    = require('./md5');
+const recursive = require('recursive-readdir');
+
+const debug = false;
 
 //let pathBase = "";
+
+/**
+ * 
+ * @param {string} file 
+ * @param {stats} stat 
+ */
+function ignoreFile(file, stat) {
+    let extName = path.extname(file);
+
+    if (stat.isDirectory()) {
+        return false;
+    }
+
+    if (extName == ".jpg") {
+        return false;
+    }
+
+    return true;
+}
 
 /**
  * 
@@ -19,30 +41,30 @@ function getPathHash(pathToSearch) {
 
         console.log("getPathHash(" + absPath + ")")
 
-        fs.readdir(absPath, (err, files) => {
+        recursive(absPath, [ ignoreFile ], (err, files) => {
             let hashMap = {}
-            console.log("Got files!")
-            files.forEach((file) => {
-                let fullpath = path.format( {
-                    dir: absPath,
-                    base: file
-                })
+            //console.log("Got files!")
+            files.forEach((fullpath) => {
+                // let fullpath = path.format( {
+                //     dir: absPath,
+                //     base: file
+                // })
 
                 let status   = fs.statSync(fullpath)
 
-                if (status.isFile()) {
+                // if (status.isFile()) {
                     let pathHash = md5(fullpath)
 
-                    console.log(fullpath)
-                    console.log(pathHash)
+                    if (debug == true) {
+                        console.log(pathHash + " : " + fullpath)
+                    }
 
                     hashMap[pathHash] = { 
                         fullPath: fullpath, 
-                        fileName: file
+                        fileName: path.basename(fullpath),
+                        fileSize: status.size
                     };
-                } else if (status.isDirectory()) {
-                    console.log("-- found directory!")
-                }
+//                }
             })
             resolve(hashMap)
         })
--- javascript/express/video-stream/test_getpath.js
+++ javascript/express/video-stream/test_getpath.js
@@ -3,8 +3,14 @@
 const getPathHash = require('./getpathhash');
 
 
-let pathHashPromise = getPathHash(".");
+let pathHashPromise = getPathHash("C:\\Users\\muman\\OneDrive\\Pictures\\");
 
 pathHashPromise.then((pathHash) => {
     console.log("promise filled!")
+    //console.log(pathHash);
+
+    Object.keys(pathHash).forEach((hash) =>{
+        console.log("Hash " + hash + " -> " + pathHash[hash].fullPath);
+    })
+
 })
