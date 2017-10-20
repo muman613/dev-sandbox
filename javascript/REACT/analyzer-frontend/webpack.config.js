f49b91665b365549f469b0830dda29f3ae3f6022
Michael Uman <Michael_Uman@sigmadesigns.com>
2017-10-19 18:43:05 -0700

    Check in analyzer front-end workspace

---
+++ javascript/REACT/analyzer-frontend/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.tsx',
+    target: "electron",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        loaders: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, loader: 'ts-loader' },
+            { test: /\.css$/, loader: 'css-loader' }
+        ]
+    }
+}
